09d08662e8143812ef02dfccdf5aa4fb8fb4e3a9
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-10-23 01:50:44 +0530

    Implemented Feature: Toggle Sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,9 +1,24 @@
+import { PanelLeftClose } from "lucide-react";
+
 import { cn } from "@/lib/utils";
+import { useStore } from "@/store";
 import SidebarItem from "./sidebar-item";
 
 export const Sidebar = () => {
+  const { showSideBar, setShowSideBar } = useStore();
+
   return (
-    <aside className={cn("w-full p-4")}>
+    <aside className={cn("relative h-full p-4 w-full")}>
+      {/* Display Show SideBar toggle button */}
+      <div className={cn("absolute z-50 top-2 right-2")}>
+        <button className="p-2 py-2 rounded-md cursor-pointer hover:bg-white/10">
+          <PanelLeftClose
+            className="w-6 h-6 text-muted-foreground"
+            onClick={() => setShowSideBar(!showSideBar)}
+          />
+        </button>
+      </div>
+
       <SidebarItem />
     </aside>
   );
