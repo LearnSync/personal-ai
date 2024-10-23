edbdc77941da8ba2d705d4be4b58a68315be30fe
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-10-24 02:36:18 +0530

    re-factoring

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,15 @@
-import { DEFAULT_EXTENSIONS_ITEMS } from "@/constants";
 import { create } from "zustand";
 
 interface StoreState {
   showSideBar: boolean;
-  activeExtensionTab: string;
-  setActiveExtensionTab: (tab: string) => void;
   setShowSideBar: (value: boolean) => void;
 }
 
 export const useStore = create<StoreState>((set) => ({
   // Get
-  activeExtensionTab: DEFAULT_EXTENSIONS_ITEMS[0].label,
   showSideBar: true,
 
   // Set
-  setActiveExtensionTab: (tab: string) => set({ activeExtensionTab: tab }),
   setShowSideBar: (value: boolean) => set({ showSideBar: value }),
 }));
 
