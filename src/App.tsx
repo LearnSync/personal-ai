bda0ffdc134e796c2a1c0f1c1676e8da1c013ffb
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-10-29 21:48:44 +0530

    re-factoring

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,7 @@ function App() {
   const { showSideBar, setShowSideBar } = useStore();
 
   // Context
-  const { activeExtensionTab, sessionManager, workbenchTabs } =
-    usePlatformContext();
+  const { activeExtensionTab, workbenchTabs } = usePlatformContext();
 
   return (
     <ThemeProvider defaultTheme="system">
