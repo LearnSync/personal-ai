1417f2ceb9e36ce12694ab8930ec830b6f64b796
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-11-30 02:38:32 +0530

    Updating the Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,6 +59,10 @@ export default {
           4: "hsl(var(--chart-4))",
           5: "hsl(var(--chart-5))",
         },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
+        },
       },
       fontFamily: {
         primary: "var(--primary-font)",
