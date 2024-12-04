2440a4db8ca7d66cd8ca4812845195e215af73d5
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-12-05 02:52:28 +0530

    Adding new Icons

--- src/constants/icon.tsx
+++ src/constants/icon.tsx
@@ -107,6 +107,29 @@ export const LocalLLMIcon: React.FC<IIconProps> = ({ className }) => {
   return <Laptop className={cn("", className)} />;
 };
 
+export const TickIcon: React.FC<IIconProps> = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width="100"
+      height="100"
+      viewBox="0 0 48 48"
+      className={cn("", className)}
+    >
+      <path
+        fill="#d8edd8"
+        d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
+      ></path>
+      <path
+        fill="hsl(142.1 76.2% 36.3%)"
+        d="M34.586,14.586l-13.57,13.586l-5.602-5.586l-2.828,2.828l8.434,8.414l16.395-16.414L34.586,14.586z"
+      ></path>
+    </svg>
+  );
+};
+
 export function getIconByIconKey({
   key,
   className,
