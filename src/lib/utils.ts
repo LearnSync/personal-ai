f87549ad9ad7dc926ab151b8a086d1d660e5d93b
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-12-05 02:52:41 +0530

    Implementing new `utils`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function openExternalLink(url: string) {
+  window.open(url, "_blank");
+}
