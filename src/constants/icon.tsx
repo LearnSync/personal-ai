0f6e7f3366c36732f024b0f4753d3a71339a1c5d
SOUMITRO-SAHA <soumitrosahaofficial@gmail.com>
2024-12-15 02:49:35 +0530

    Adding new Icon

--- src/constants/icon.tsx
+++ src/constants/icon.tsx
@@ -1,5 +1,7 @@
 import {
   Blocks,
+  Code,
+  FileCode2Icon,
   FileStack,
   Laptop,
   MessageCircle,
@@ -130,6 +132,27 @@ export const TickIcon: React.FC<IIconProps> = ({ className }) => {
   );
 };
 
+export const NotificationDotIcon: React.FC<IIconProps> = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={cn("lucide lucide-bell-dot", className)}
+    >
+      <path d="M10.268 21a2 2 0 0 0 3.464 0" />
+      <path d="M13.916 2.314A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.74 7.327A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673 9 9 0 0 1-.585-.665" />
+      <circle cx="18" cy="8" r="3" />
+    </svg>
+  );
+};
+
 export function getIconByIconKey({
   key,
   className,
@@ -179,8 +202,10 @@ export function getIconByKey(key: string | undefined): React.ReactNode {
   if (!key) return <MessageCircle className="w-full h-full" />;
 
   switch (key) {
-    case EXTENSION_KEY.CHAT:
-      return <MessageCircle className="w-full h-full" />;
+    case EXTENSION_KEY.CHAT_WITH_PDF:
+      return <FileCode2Icon className="w-full h-full" />;
+    case EXTENSION_KEY.CODE:
+      return <Code className="w-full h-full" />;
     case EXTENSION_KEY.CONTEXT_SEARCH:
       return <Search className="w-full h-full" />;
     case EXTENSION_KEY.IMPORTANT_CHAT:
